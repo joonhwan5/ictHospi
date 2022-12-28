069bf4fe7001cad9341bbad55d6b10aa74eee568
doyoung0806 <dylim0806@naver.com>
2022-12-28 17:37:45 +0900

    20221228 임도영 (introController,doctormapper 추가)

--- src/main/java/com/spring/hospital/controller/introController.java
+++ src/main/java/com/spring/hospital/controller/introController.java
@@ -1,25 +1,30 @@
 package com.spring.hospital.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.hospital.introduce.IIntroDoctorsService;
+
 @Controller
-@RequestMapping("introduce")
+@RequestMapping("/introduce")
 public class introController {
+	
+	@Autowired
+	private IIntroDoctorsService service;
 	
 	@GetMapping("/introMain")
 	public void introduce() {
 		
 	}
 	
+	//각 의사 소개페이지로 이동
 	@GetMapping("/introDoctors")
-	public void introDoctors() {
-		
+	public void introDoctors(String doctorName, String medicalDepartment) {
+		service.getDoctorInfo(doctorName, medicalDepartment);
 	}
 	
-	@GetMapping("/introCome")
-	public void introCome() {
-		
-	}
+	//의사 정보 화면에 뿌려주기
+	
 }
